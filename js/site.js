624021b9204c9ce566ecf4e715c71c03c79e2039
Kevin Foley <kfoley@squarespace.com>
2014-07-15 21:45:03 -0400

    Registering service worker.

--- js/site.js
+++ js/site.js
@@ -1,5 +1,15 @@
 (function () {
 
-  // service worker shit goes here.
+  // force https
+  if ((!location.port || location.port == "80") && location.protocol != 'https:') {
+    location.protocol = 'https:';
+  }
+
+  // Register the service worker.
+  if ('serviceWorker' in navigator) {
+    test = navigator.serviceWorker.register('/js/sw.js', {
+      scope: '/*'
+    });
+  }
 
 }());
